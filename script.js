263b86394775ce8ce927e3fa31f836588b91e27c
Komalpreet Singh <komalpreetsingh2305@gmail.com>
2023-11-02 03:05:03 -0400

    fixed some bugs

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const options = {
 
 let city = "Brampton";
 
-
+let yes = "check";
 const getWeather = (city) => {
     cityName.innerHTML = city;
     fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options).then(response => response.json()).then((response) => {
-        cloud_pct.innerHTML = response.cloud_pct
+        //cloud_pct.innerHTML = response.cloud_pct
         feels_like.innerHTML = response.feels_like
         humidity.innerHTML = response.humidity
         max_temp.innerHTML = response.max_temp
@@ -24,7 +24,7 @@ const getWeather = (city) => {
         wind_degrees.innerHTML = response.wind_degrees
         wind_speed.innerHTML = response.wind_speed
 
-    }).catch(err => console.error(err));
+    }).catch(err => document.body.appendChild(yes));
 }
 
 let subBtn = document.getElementById('submit');
---
+++ test.js
@@ -0,0 +1,6 @@
+// let ok = test.innerHTML = "Komal";
+// document.body.appendChild(ok);
+
+console.log(test);
+console.log(para);
+console.log(p2.innerHTML);
